4a96c5c6ff5b3b67960b8ce8a911a1f7f90b8175
Ljulienng <ljuliennguyen@gmail.com>
2021-11-23 15:33:15 +0100

    Comments are now in english, small tweaks done

--- srcs/server/Server.cpp
+++ srcs/server/Server.cpp
@@ -1,5 +1,46 @@
 #include "Server.hpp"
 
+// Basic socket initialization following the basic steps
+int Server::start()
+{
+	// Socket
+	_sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (_sockfd == -1)
+	{
+		std::cerr << "Failed to create socket." << std::endl; // Temporary messages
+		return (EXIT_FAILURE);
+	}
+	// Bind
+	initAddr();
+	if (bind(_sockfd, (struct sockaddr *)&_sockaddr, sizeof(_sockaddr) < 0))
+	{
+		std::cerr << "Failed to bind" << std::endl;
+		return (EXIT_FAILURE);
+	}
+	// Listen
+	if (listen(_sockfd, 100) < 0) // Maximum can be higher, to be tested
+	{
+		std::cerr << "Failed to listen on socket" << std::endl;
+		return (EXIT_FAILURE);
+	}
+	// Start (Not in this function)
+	return (EXIT_SUCCESS);
+}
+
+// Filling the sockaddr_in variable
+void Server::initAddr()
+{
+	_sockaddr.sin_family = AF_INET;
+	_sockaddr.sin_addr.s_addr = htonl(HOST);
+	_sockaddr.sin_family = htons(PORT);
+	// PORT & HOST from the config file
+}
+
+int	Server::getSockfd()
+{
+	return (_sockfd);
+}
+
 Server::Server()
 {
 }
@@ -15,43 +56,10 @@ Server::~Server()
 
 Server &Server::operator=(const Server &src)
 {
+	if (&src == this)
+		return (*this);
 	_sockaddr = src._sockaddr;
 	_sockfd = src._sockfd;
 
 	return (*this);
-}
-
-int Server::start() //Initialisation du socket
-{
-	// Socket
-	_sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	if (_sockfd == -1)
-	{
-		std::cerr << "Failed to create socket." << std::endl; // Message temporaire
-		return (EXIT_FAILURE);
-	}
-	// Bind
-	fillAddr();
-	if (bind(_sockfd, (struct sockaddr *)&_sockaddr, sizeof(_sockaddr) < 0))
-	{
-		std::cerr << "Failed to bind" << std::endl;
-		return (EXIT_FAILURE);
-	}
-	// Listen
-	if (listen(_sockfd, 100) < 0) // Maximum modifiable à voir
-	{
-		std::cerr << "Failed to listen on socket" << std::endl;
-		return (EXIT_FAILURE);
-	}
-	// Start à faire dans une autre fonction
-	return (EXIT_SUCCESS);
-}
-
-void Server::fillAddr()
-{
-	memset((char *)&_sockaddr, 0, sizeof(_sockaddr));
-	_sockaddr.sin_family = AF_INET;
-	_sockaddr.sin_addr.s_addr = htonl(HOST);
-	_sockaddr.sin_family = htons(PORT);
-	// PORT ET HOST à définir
 }
