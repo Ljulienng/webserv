c6c13348506845fe3c6477cc26e10914574a16c2
Ljulienng <ljuliennguyen@gmail.com>
2021-12-09 16:10:04 +0100

    Parsing done only body parsing to be finished

--- webserv.mk
+++ webserv.mk
@@ -3,17 +3,19 @@ SRC_FILES	= 	webserv.cpp					\
 				location.cpp				\
 				server.cpp					\
 				hub.cpp						\
+				request.cpp					\
 				clientSocket.cpp			\
 				socket.cpp					\
 				file.cpp					\
-				str.cpp	
+				str.cpp						
 
 INC_FILES	= 	srcs/webserv.hpp									\
 				srcs/config/configuration/configuration.hpp			\
 				srcs/config/location/location.hpp					\
 				srcs/config/server/server.hpp						\
 				srcs/config/hub.hpp									\
-				srcs/socket/clientSocket.hpp								\
+				srcs/socket/clientSocket.hpp						\
 				srcs/socket/socket.hpp								\
+				srcs/config/request/request.hpp						\
 				srcs/utils/file/file.hpp							\
 				srcs/utils/str/str.hpp
