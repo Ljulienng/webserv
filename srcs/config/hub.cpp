2d25cff9356351fd691fcf71c5924fc57420647c
eteillet <eteillet@student.42.fr>
2021-12-12 14:51:19 +0100

    update hub.cpp

--- srcs/config/hub.cpp
+++ srcs/config/hub.cpp
@@ -12,7 +12,6 @@ void	Hub::start()
 void	Hub::process()
 {
 	int pollRet = 1;
-	int acceptRet = 0;
 		
 	// call poll and wait an infinite time
 	pollRet = poll(_config.getFds(), _config.getNfds(), -1);
@@ -30,30 +29,10 @@ void	Hub::process()
 		if (_config.getFds()[i].revents == POLLIN)
 		{
 			std::cout << i <<  " POLLIN\n";
-			// if the current fd is one of our servers, we connect a new client
-			// listening descriptor is readable
+			// if the current fd is one of our servers, we connect a new client (listening descriptor is readable)
 			if (i < _config.getServers().size()) // fd stored after "nb of servers" are clients fd and not servers
 			{
-				// accept all incoming connections that are queued up on the listening socket before
-				// accept each incoming connection
-				while (42)
-				{
-					acceptRet = accept(_config.getFds()[i].fd, NULL, NULL);
-					if (acceptRet == -1) // no connection is present
-						break ;
-					ClientSocket client;
-					client.setFd(acceptRet);
-					client.setPort(_config.getServers().find(i)->second.getPort());
-					// insert new client at index 'i + nb of servers' to find it easily
-					_config.getClients().insert(std::pair<size_t, ClientSocket>(i + _config.getServers().size(), client));
-					// add the new incoming connection to the pollfd structure
-					std::cout << "New incoming connection - fd : " << acceptRet << "\n";
-					std::cout << "nfds = " << _config.getNfds()<< "\n";
-					_config.getFds()[_config.getNfds()].fd = acceptRet;
-					_config.getFds()[_config.getNfds()].events = POLLIN;
-					_config.setNfds(_config.getNfds() + 1);
-					// loop back up and accept another incoming connection
-				}
+				_acceptIncomingConnections(i);
 			}
 			// it's not a listening socket (server), therefore an existing connection must be readable (client)
 			// it's a client so we need to receive the request (recv), parse it and prepare response
@@ -67,7 +46,7 @@ void	Hub::process()
 					throw std::string("Error: can't receive client request");
 				else if (bytes > 0)
 				{
-					_config.getClients().find(i)->second.getBuffer().append(buffer.begin(), buffer.end()); // modif access map ELIE
+					_config.getClients()[i].getBuffer().append(buffer.begin(), buffer.end());
 					while (bytes == MAX_BUF_LEN)
 					{
 						buffer.clear();
@@ -75,104 +54,120 @@ void	Hub::process()
 						if (bytes < 0)
 							throw std::string("Error: can't receive client request");
 						else
-							_config.getClients().find(i)->second.getBuffer().append(buffer.begin(), buffer.end()); // modif access map ELIE
+							_config.getClients()[i].getBuffer().append(buffer.begin(), buffer.end());
 					}
 				}
 				else
 					bytes = 0;
 				_config.getClients()[i].addRequest();
-
-
-				/********** TEST A SUPPR AVANT PUSH ***************/
-				// std::vector<char>	bufRequest(2048);
-				// int bytesRequest = recv(_config.getFds()[i].fd, &bufRequest[0], 2048, 0);
 				
-				// if (bytesRequest < 0)
-				// 	exit(EXIT_FAILURE);
-				/*************************************************/
-					
 
 				// PARSE THE REQUEST :
 						// - push de la requete dans le SocketClient
-
-				// PREPARE THE RESPONSE :
-						// - recupere la derniere requete (top)
-						// - verifie erreurs
-						// - construit reponse
-						// - push reponse dans le socketClient
-						// - supprime requete(pop)
-						
-				/********** TEST A SUPPR AVANT PUSH ***************/
-				Response resp;
-				resp.setContent("TEST"); // recuperer le buffer du client
-				if (_config.getClients().find(i) != _config.getClients().end())
-				{
-					_config.getClients().find(i)->second.getResponses().push(resp);
-					// the socket is now ready to write in addition to reading because we have added a response
-					if (_config.getClients().find(i)->second.getResponses().empty() == false)
-						_config.getFds()[i].events = POLLIN | POLLOUT;
-				}
-				/*************************************************/		
+	
+				_prepareResponse(i);
 			}
 		}
 		else if (_config.getFds()[i].revents == POLLOUT)
 		{
 			std::cout << i <<  " POLLOUT\n";
-			// SEND RESPONSE [ get responses queue of the client, send them and delete them ]
-			
-			std::map<size_t, ClientSocket>::iterator	client = _config.getClients().find(i);
-			std::queue<Response>    					responses = client->second.getResponses();
-			std::string									buffer/* = client->second.getBuffer()*/;
-
-			// we process the responses one by one and append them to the client buffer
-			// then delete the response until the queue is empty
-			while (responses.empty() == false)
-			{
-				// process in FIFO order, we process the oldest element first
-				Response response = responses.front();
-
-				// then have to check header and status of the response
-				// ...
-
-				// put into a string the response
-				std::string		message = response.getMessage();
-				// std::cout << "message to write in socket : \n" << message << "\n";
-				// append it to the client buffer
-				buffer.insert(buffer.end(), message.begin(), message.end());
-
-				// delete the response
-				responses.pop();
-			}
-
-			if (buffer.empty() == false)
-			{
-				// we can use send() ( without flag parameter, send is equivalent to write() )
-				// so write into the _fds[i].fd the content of buffer
-				// int bytes = send(_config.getFds()[i].fd, &buffer[0], buffer.size(), 0);
-				// ou ...
-				int bytes = write(_config.getFds()[i].fd, &buffer[0], buffer.size());
-
-				if (bytes <= 0)
-				{
-					// disconnect each sockets (server and clients) and exit
-					exit(EXIT_FAILURE);
-				}
-				// clear the buffer
-				buffer.erase(buffer.begin(), buffer.begin() + bytes);
-				// ok the response is sent !!!
-
-				// we finished to write so we are now waiting for reading
-				_config.getFds()[i].events = POLLIN;
-			}
-		
+			_sendResponse(i);
 		}
 		else
 		{
-			// ERROR QUIT PROGRAM
+			std::cout << i <<  " ERROR QUIT PROGRAM\n";
+			exit(EXIT_FAILURE);
 		}
 	}
 }
 
+/*
+** accept client :
+** accept all incoming connections that are queued up on the listening socket before
+*/
+void		Hub::_acceptIncomingConnections(size_t index)
+{
+	while (42)
+	{
+		int acceptRet = accept(_config.getFds()[index].fd, NULL, NULL);
+
+		if (acceptRet == -1) // no connection is present
+			break ;
+
+		ClientSocket client;
+		client.setFd(acceptRet);
+		client.setPort(_config.getServers().find(index)->second.getPort());
+		// insert new client at index 'i + nb of servers' to find it easily
+		_config.getClients().insert(std::pair<size_t, ClientSocket>(index + _config.getServers().size(), client));
+		
+		// add the new incoming connection to the pollfd structure
+		std::cout << "New incoming connection - fd : " << acceptRet << "\n";
+		std::cout << "nfds = " << _config.getNfds()<< "\n";
+		_config.getFds()[_config.getNfds()].fd = acceptRet;
+		_config.getFds()[_config.getNfds()].events = POLLIN;
+		_config.setNfds(_config.getNfds() + 1);
+		// loop back up and accept another incoming connection
+	}
+}
+
+/*
+** prepare response :
+**	- get the last request
+**  - check errors
+**  - build the response
+**  - add the response in socketClient
+**  - delete the request
+*/
+void		Hub::_prepareResponse(size_t index)
+{
+	Response resp;
+	resp.setContent("TEST"); // recuperer le buffer du client
+	if (_config.getClients().find(index) != _config.getClients().end())
+	{
+		_config.getClients().find(index)->second.getResponses().push(resp);
+		// the socket is now ready to write in addition to reading because we have added a response
+		if (_config.getClients().find(index)->second.getResponses().empty() == false)
+			_config.getFds()[index].events = POLLIN | POLLOUT;
+	}
+}
+
+void 		Hub::_sendResponse(size_t index)
+{
+	// SEND RESPONSE [ get responses queue of the client, send them and delete them ]
+	std::map<size_t, ClientSocket>::iterator	client = _config.getClients().find(index);
+	std::queue<Response>    					responses = client->second.getResponses();
+	std::string									buffer/* = client->second.getBuffer()*/;
+
+	// we process the responses one by one and append them to the client buffer
+	// then delete the response until the queue is empty
+	while (responses.empty() == false)
+	{
+		// process in FIFO order, we process the oldest element first
+		Response response = responses.front();
+
+		// then have to check header and status of the response
+		// ...
+
+		std::string		message = response.getMessage(); // put into a string the response
+		buffer.insert(buffer.end(), message.begin(), message.end()); // append it to the client buffer
+
+		responses.pop(); // delete the response
+	}
+
+	if (buffer.empty() == false)
+	{
+		// we can use send() ( without flag parameter, send is equivalent to write() )
+		// so write into the _fds[i].fd the content of buffer
+		int bytes = write(_config.getFds()[index].fd, &buffer[0], buffer.size());
+
+		if (bytes <= 0)
+			exit(EXIT_FAILURE); // disconnect each sockets (server and clients) and exit
+		buffer.erase(buffer.begin(), buffer.begin() + bytes); // clear the buffer
+
+		// finished to write so we are now waiting for reading
+		_config.getFds()[index].events = POLLIN;
+	}
+}
 
 /* GETTERS */
 Configuration	&Hub::getConfig()
